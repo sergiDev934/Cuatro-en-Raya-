32cf476fba0d53d5c1d5f05673476ddaa7396149
Sergio <programayaprende422@gmail.com>
2024-08-07 21:10:26 +0200

    Combos del win hechos

--- src/App.jsx
+++ src/App.jsx
@@ -6,18 +6,52 @@ const TURNS = {
   O: "o",
 };
 
+const COMBO_WINS = [
+  [0, 1, 2, 3],
+  [1, 2, 3, 4],
+  [5, 6, 7, 8],
+  [6, 7, 8, 9],
+  [10, 11, 12, 13],
+  [11, 12, 13, 14],
+  [15, 16, 17, 18],
+  [16, 17, 18, 19],
+  [0, 5, 10, 15],
+  [1, 6, 11, 16],
+  [2, 7, 12, 17],
+  [3, 8, 13, 18],
+  [4, 9, 14, 19],
+  [0, 6, 12, 18],
+  [1, 7, 13, 19],
+  [3, 7, 11, 15],
+  [4, 8, 12, 16],
+];
+
 export function App() {
   const [turn, setTurn] = useState(TURNS.X);
-  const [board, setBoard] = useState(Array(42).fill(null));
+  const [board, setBoard] = useState(Array(20).fill(null));
+  const [winner, setWinner] = useState(null); //FALSE EMPATE
 
   const checkDown = (index) => {
-    if (board[index + 7] !== null) {
+    if (board[index + 5] !== null) {
       const newBoard = board;
       newBoard[index] = turn;
       setBoard(newBoard);
     } else {
-      checkDown(index + 7);
+      checkDown(index + 5);
     }
+  };
+
+  const checkWinner = () => {
+    COMBO_WINS.map((combo) => {
+      const [a, b, c, d] = combo;
+      if (
+        board[a] === board[b] &&
+        board[a] === board[c] &&
+        board[a] === board[d]
+      )
+        return board[a];
+    });
+    return null;
   };
 
   const updateBoard = (index) => {
@@ -25,6 +59,10 @@ export function App() {
 
     //cambiar tablero
     checkDown(index);
+
+    // comprobar ganador
+    const posibleWinner = checkWinner();
+    if (posibleWinner) setWinner(true);
 
     // cambio de turno
     const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X;
